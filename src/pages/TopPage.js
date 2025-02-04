15d87276c9d30925f78ccef7f4b8795ae140f4a4
mina37010 <itomita65@gmail.com>
2025-02-04 14:22:33 +0900

    toppage_update

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -5,6 +5,7 @@ const TopPage = () => {
     <div>
       <div className='center'>
         <h1>開発中だよん！</h1>
+        <h2><a className="a-non" href="https://asaka.party/party">🎉</a></h2>
       </div> 
     </div>
   );
