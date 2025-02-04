9374ff05c4de5808fd47e54bce4cc1b8a388617f
mina37010 <itomita65@gmail.com>
2025-02-04 20:34:00 +0900

    add nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import asaka from '../assets/images/asaka.jpeg';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <a className="a-non" href="/">
+      <div className="logo-container" >
+      <img src={asaka} alt="asaka" width="100px" height="100px" />
+        <h1 className="site-name"><font color="red">A</font>saka.party!</h1>
+      </div>
+      </a>
+      <ul className="nav-links">
         <li><Link to="/">TopPage</Link></li>
         <li><Link to="/210on">皆様と一緒</Link></li>
       </ul>
